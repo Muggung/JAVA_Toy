c191cee146e229a938e532b1430661f9fb6c02d9
Dong <117351842+Muggung@users.noreply.github.com>
2023-04-23 17:54:07 +0900

    Delete employee_manage directory

--- employee_manage/src/com/employee_manage/model/common/Employee_Controller.java
+++
@@ -1,6 +0,0 @@
-package com.employee_manage.model.common;
-
-public interface Employee_Controller {
-	void mainMenu();
-	void printAllEmployee();
-}
--- employee_manage/src/com/employee_manage/run/Run.java
+++
@@ -1,9 +0,0 @@
-package com.employee_manage.run;
-
-import com.employee_manage.controller.Controller;
-
-public class Run {
-	public static void main(String[] args) {
-		new Controller().mainMenu();
-	}
-}
